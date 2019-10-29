438936267156ad5f1cea4073f3f49ed2ee617326
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-29 15:00:12 +0100

    router succesfully connected

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-// const router = require('./posts/posts-router');
+const router = require('./posts/posts-router');
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/posts", router)
 
 server.get('/', (req, res) => {
     res.send('Node is running')
