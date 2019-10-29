de042b99fb156bde9eb3c2f72b096c660c9849b9
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-29 15:59:11 +0100

    POST endpoint completed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,22 +1,49 @@
-const express = require('express');
-const db = require('../data/db');
+const express = require("express");
+const db = require("../data/db");
 
 const router = express.Router();
 
-router.get("/", getAllPosts)
+router.post("/", postUser);
+router.get("/", getAllPosts);
 
-function getAllPosts(req, res) {
- db.find()
-  .then(posts => {
-      res.status(200).json(posts)
-  })
-  .catch(error => {
+function postUser(req, res) {
+ 
+  const newPost = req.body;
+  if (!newPost.title || !newPost.contents) {
+    return res.status(400).json({
+      success: false,
+      errorMessage: "Please provide title and contents for the post"
+    });
+  }
+  db.insert(newPost)
+    .then(post => {
+      db.findById(post.id).then(user => {
+        res.status(200).json({
+          success: true,
+          user
+        });
+      });
+    })
+    .catch(error => {
       res.status(500).json({
-          success:false,
-          error: "The post information could not be retireved"
-      })
-  })
+        success: false,
+        error,
+        errorMessage: "There was an error while saving the post to the database"
+      });
+    });
 }
 
+function getAllPosts(req, res) {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        success: false,
+        error: "The post information could not be retireved" + " " + error
+      });
+    });
+}
 
 module.exports = router;
